b2c41f7defb4adee107fdd5c95f1e06d1e179c86
Siew Wing Fei <wingfei.siew@nova-hub.com>
2022-08-18 11:45:15 +0800

    i

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,16 +1,31 @@
-
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { CreateLessonInput } from "./lesson.input";
+import { LessonService } from "./lesson.service";
 import { LessonType } from "./lesson.type";
 
 @Resolver(of => LessonType)
 export class LessonResolver {
+
+  constructor(
+    private lessonService: LessonService,
+  ) {}
+
   @Query(returns => LessonType)
-  lesson() {
-    return {
-      id: 'xx',
-      name: 'vvv',
-      startDate: (new Date()).toISOString(),
-      endDate: (new Date()).toISOString(),
-    }
+  async lesson(
+    @Args('id') id: string,
+  ) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Query(returns => [LessonType])
+  async lessons() {
+    return this.lessonService.getLessons();
+  }
+
+  @Mutation(returns => LessonType)
+  async createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
